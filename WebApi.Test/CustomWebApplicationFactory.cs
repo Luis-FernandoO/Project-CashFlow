b56b53c04e4e0529c4fb87984b69623fa0bcc825
luis <contatoluisfernandogaleano@gmail.com>
2025-05-13 11:28:04 -0400

    update nos tests

--- WebApi.Test/CustomWebApplicationFactory.cs
+++ WebApi.Test/CustomWebApplicationFactory.cs
@@ -1,4 +1,9 @@
-﻿using CashFlow.Infrastructure.DataAcess;
+﻿using CashFlow.Domain.Entities;
+using CashFlow.Domain.Security.Cryptography;
+using CashFlow.Infrastructure.DataAcess;
+using CommonTestUtilities.Entities;
+using DocumentFormat.OpenXml.Office.CustomXsn;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
@@ -8,6 +13,9 @@ namespace WebApi.Test;
 
 public class CustomWebApplicationFactory : WebApplicationFactory<Program>
 {
+    private User _user;
+    private string _password;
+
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.UseEnvironment("Test")
@@ -20,7 +28,26 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
                     config.UseInMemoryDatabase("InMemoryDbForTesting");
                     config.UseInternalServiceProvider(provider);
                 });
+                var scope = services.BuildServiceProvider().CreateScope();
+                var dbContext = scope.ServiceProvider.GetRequiredService<CashFlowDbContext>();
+                var passwordEncripter = scope.ServiceProvider.GetRequiredService<IPasswordEncripter>();
+
+                StartDataBase(dbContext, passwordEncripter);
             });
+    }
+    
+    public string GetEmail() => _user.Email;
+    public string GetName() => _user.Name;
+    public string GetPassword() => _password;
+    private void StartDataBase(CashFlowDbContext context, IPasswordEncripter passwordEncripter)
+    {
+        _user = UserBuilder.Build();
+        _password = _user.Password;
+
+        _user.Password = passwordEncripter.Encrypt(_user.Password);
+
+        context.Users.Add(_user);
+        context.SaveChanges();
     }
 
 }
--- WebApi.Test/Login/DoLogin/DoLoginTest.cs
+++ WebApi.Test/Login/DoLogin/DoLoginTest.cs
@@ -1,8 +1,63 @@
-﻿namespace WebApi.Test.Login.DoLogin;
+﻿using CashFlow.Commnication.Requests;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace WebApi.Test.Login.DoLogin;
 public class DoLoginTest : IClassFixture<CustomWebApplicationFactory>
 {
-    private const string 
+    private const string METHOD = "api/Login";
+
+    private readonly HttpClient _httpClient;
+    private readonly string _email;
+    private readonly string _name;
+    private readonly string _password;
 
 
+    public DoLoginTest(CustomWebApplicationFactory webApplicationFactory)
+    {
+        _httpClient = webApplicationFactory.CreateClient();
+        _email = webApplicationFactory.GetEmail();
+        _name = webApplicationFactory.GetName();
+        _password = webApplicationFactory.GetPassword();
 
+    }
+
+    [Fact]
+    public async Task Success()
+    {
+        var request = new RequestLoginJson
+        {
+            Email = _email,
+            Password =  _password
+        };
+
+        var response = await _httpClient.PostAsJsonAsync(METHOD, request);
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var responseBody = await response.Content.ReadAsStreamAsync();
+
+        var responseData = await JsonDocument.ParseAsync(responseBody);
+
+        responseData.RootElement.GetProperty("name").GetString().Should().Be(_name);
+        responseData.RootElement.GetProperty("token").GetString().Should().NotBeNullOrWhiteSpace();
+
+    }
+
+    [Fact]
+    public async Task Error_Login_Invalid()
+    {
+        var request = RequestLoginJsonBuilder.Build();
+        
+        var response = await _httpClient.PostAsJsonAsync(METHOD, request);
+        response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);   
+        var responseBody = await response.Content.ReadAsStreamAsync();
+        var responseData = await JsonDocument.ParseAsync(responseBody);
+        var errors = responseData.RootElement.GetProperty("errorMessages").EnumerateArray();
+
+        errors.Should().HaveCount(1).And.Contain(c => c.GetString()!.Equals("Email ou Senha Inválidos"));
+    }
 }
